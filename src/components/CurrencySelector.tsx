3489e7ee24bc035f6acdcda1318df4be000a08f4
Ofir Valla <44082893+OfirValla@users.noreply.github.com>
2026-01-04 17:00:25 +0200

    On currency change show "no convertion" notification

--- src/components/CurrencySelector.tsx
+++ src/components/CurrencySelector.tsx
@@ -3,6 +3,7 @@ import { Label } from '@/components/ui/label';
 import { Select } from '@/components/ui/select';
 import { usePlans } from '@/context/PlanProvider';
 import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
 
 export function CurrencySelector() {
   const { t } = useTranslation('portfolio-page');
@@ -19,7 +20,10 @@ export function CurrencySelector() {
       </Label>
       <Select
         value={currency}
-        onValueChange={(value) => setCurrency(value as CurrencyCode)}
+        onValueChange={(value) => {
+          setCurrency(value as CurrencyCode);
+          toast.info("The site does not automatically convert between currencies");
+        }}
         options={currencyOptions}
         className="w-[140px] sm:w-[180px]"
       />
